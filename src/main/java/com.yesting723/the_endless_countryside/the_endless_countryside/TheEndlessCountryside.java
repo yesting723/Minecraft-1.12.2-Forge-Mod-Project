1c9ee2dd59080b9cc3b06aad320d48a35a4523ef
yesting723 <104178500+yesting723@users.noreply.github.com>
2022-07-08 15:53:39 +0800

    添加主类

---
+++ src/main/java/com.yesting723/the_endless_countryside/the_endless_countryside/TheEndlessCountryside.java
@@ -0,0 +1,16 @@
+package com.the_endless_countryside.the_endless_countryside;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = TheEndlessCountryside.MODID, name = TheEndlessCountryside.NAME, version = TheEndlessCountryside.VERSION)
+public class TheEndlessCountryside
+{
+    public static final String MODID = "the_endless_countryside";
+    public static final String NAME = "The Endless Countryside";
+    public static final String VERSION = "0.0.8";
+}
