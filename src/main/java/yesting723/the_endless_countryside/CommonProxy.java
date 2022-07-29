3ade00cac21e738fc70b6f21705a437ded6fcf12
yesting723 <104178500+yesting723@users.noreply.github.com>
2022-07-29 12:20:14 +0800

    Add files via upload

---
+++ src/main/java/yesting723/the_endless_countryside/CommonProxy.java
@@ -0,0 +1,16 @@
+package yesting723.the_endless_countryside.proxy;
+
+import yesting723.the_endless_countryside.register.*;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class CommonProxy {
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+}
---
+++ src/main/java/yesting723/the_endless_countryside/TheEndlessCountryside.java
@@ -0,0 +1,5 @@
+package yesting723.the_endless_countryside;
+
+public class TheEndlessCountryside
+{
+}
